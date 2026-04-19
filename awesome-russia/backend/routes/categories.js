89966e40936c36b161a8e14a4800e5988c53b108
lowwin-dev <low111win@gmail.com>
2026-04-20 01:22:12 +0600

    Add categories route to fetch all categories

--- awesome-russia/backend/routes/categories.js
+++ awesome-russia/backend/routes/categories.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const { getAllCategories } = require('../models/category');
 
+router.get('/', async (req, res) => {
+  const categories = await getAllCategories();
+  res.json(categories);
+});
+
+module.exports = router;
