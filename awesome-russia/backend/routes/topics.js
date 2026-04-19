78b0c1d2ca3b827c1f059a7f21086f6d92461d48
lowwin-dev <low111win@gmail.com>
2026-04-20 01:22:52 +0600

    Add routes for getting and creating topics

--- awesome-russia/backend/routes/topics.js
+++ awesome-russia/backend/routes/topics.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const { createTopic, getTopicsByCategory } = require('../models/topic');
+const authMiddleware = require('../middleware/auth');
 
+router.get('/category/:id', async (req, res) => {
+  const topics = await getTopicsByCategory(req.params.id);
+  res.json(topics);
+});
+
+router.post('/', authMiddleware, async (req, res) => {
+  const { category_id, title } = req.body;
+  const topic = await createTopic(category_id, req.user.id, title);
+  res.json(topic);
+});
+
+module.exports = router;
