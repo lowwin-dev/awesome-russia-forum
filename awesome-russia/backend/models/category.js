c09c0a86e36fe8dc0b0946859b0c6d8b21ef7a19
lowwin-dev <low111win@gmail.com>
2026-05-23 00:41:36 +0600

    Delete awesome-russia directory

--- awesome-russia/backend/models/category.js
+++
@@ -1,8 +0,0 @@
-const pool = require('../db');
-
-async function getAllCategories() {
-  const res = await pool.query('SELECT * FROM categories ORDER BY sort_order');
-  return res.rows;
-}
-
-module.exports = { getAllCategories };
